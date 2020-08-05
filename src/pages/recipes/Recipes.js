b89234a405be8310a43db8e3f83a0df66f7b0cbb
Seanbon0611 <seand0611@gmail.com>
2020-08-05 14:14:35 -0700

    updated recipes page

--- src/pages/recipes/Recipes.js
+++ src/pages/recipes/Recipes.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import RecipeCard from "../../components/recipe-card/RecipeCard";
-import VideoCard from "../../components/video-card/VideoCard";
 import api from "../../services/api";
 import "./recipes.styles.scss";
 
@@ -13,18 +12,15 @@ const RecipesPage = () => {
       .then((recipesList) => setRecipes(recipesList.recipes.data));
   }, []);
 
-  // useEffect(() => {
-  //   api.youtube
-  //     .recipesPlaylist()
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setVideos(data.items);
-  //       setLoading(false);
-  //     });
-  // }, []);
   return (
-    <div>
-      <h3>Recipes</h3>
+    <div className="recipe-container">
+      <div className="recipe-header-container">
+        <h1 className="recipe-header">Recipes</h1>
+        <div className="filters">
+          <span>Filter</span>
+          <span>Sort By</span>
+        </div>
+      </div>
       <div className="recipe-cards">
         {recipes.map((recipe) => {
           return <RecipeCard key={recipe.id} recipe={recipe} />;
