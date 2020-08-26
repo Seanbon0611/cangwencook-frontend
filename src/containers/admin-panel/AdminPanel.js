22f366e719280107df677f1045855e6d00407be2
Seanbon0611 <seand0611@gmail.com>
2020-08-25 17:47:52 -0700

    added new routes to admin panel

--- src/containers/admin-panel/AdminPanel.js
+++ src/containers/admin-panel/AdminPanel.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import RecipesPage from "../../pages/recipes/Recipes";
+import NewRecipePage from "../../pages/new-recipe/NewRecipe";
+import EditRecipe from "../../pages/edit-recipe/EditRecipe";
 
-const AdminPanel = ({ firstName, recipes }) => {
+const AdminPanel = ({ recipes }) => {
   return (
     <div>
       <Switch>
-        <Route path="/recipes" component={RecipesPage} />
+        <Route exact path="/recipes" component={RecipesPage} />
+        <Route
+          exact
+          path="/recipes/:id/edit"
+          render={(routerProps) => {
+            const recipeId = routerProps.match.params.id;
+            const recipe = recipes.find((recipe) => {
+              return recipe.id.toString() === recipeId;
+            });
+            return recipe ? <EditRecipe recipe={recipe} /> : "Loading...";
+          }}
+        />
+        <Route exact path="/recipes/new" component={NewRecipePage} />
       </Switch>
     </div>
   );
