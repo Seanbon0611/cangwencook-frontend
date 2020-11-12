1cfbfafb49a0de91b333f01b47795d4e1133632a
Seanbon0611 <seand0611@gmail.com>
2020-11-12 14:47:20 -0800

    completed mobile & desktop versions of About page and Homepage

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RecipeCard from "../../components/recipe-card/RecipeCard";
+import RecipeCarousel from "../../components/recipe-carousel/RecipeCarousel";
 import "./homepage.styles.scss";
 
 const Homepage = ({ recipes }) => {
@@ -10,7 +11,7 @@ const Homepage = ({ recipes }) => {
       <div className="image-container">
         <img
           alt="hero"
-          src="https://can-gwen-cook.s3-us-west-1.amazonaws.com/Screen+Shot+2020-10-21+at+6.14.27+PM.png"
+          src="https://can-gwen-cook.s3-us-west-1.amazonaws.com/new-hero-image.png"
         />
       </div>
       <h1>Latest Recipes</h1>
@@ -18,6 +19,9 @@ const Homepage = ({ recipes }) => {
         {topThree.map((recipe) => (
           <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
+      </div>
+      <div className="top-recipes-mobile">
+        <RecipeCarousel recipes={topThree} />
       </div>
     </div>
   );
