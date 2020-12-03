3a12ead85b6cba314b0b432ff310e39b72434357
Seanbon0611 <seand0611@gmail.com>
2020-12-03 11:48:02 -0800

    adding blog page component

---
+++ src/pages/blog/Blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./blog.styles.scss";
+
+const Blog = () => {
+  return (
+    <div className="blog-page-container">
+      <h1 className="coming-soon">COMING SOON!</h1>
+    </div>
+  );
+};
+
+export default Blog;
