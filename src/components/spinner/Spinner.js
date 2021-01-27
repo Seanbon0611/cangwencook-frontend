27dc0798342e9bd9ec24da0497f69489b0300961
Seanbon0611 <seand0611@gmail.com>
2021-01-27 15:21:21 -0800

    created a spinner component to render when elements are loading

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./spinner.styles.scss";
+
+const Spinner = () => {
+  return (
+    <div className="post loading">
+      <svg
+        width="80"
+        height="80"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="#0ca16c"
+          strokeWidth="10"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+          transform="rotate(275.845 50 50)"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            calcMode="linear"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+            dur="1s"
+            begin="0s"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </svg>
+    </div>
+  );
+};
+
+export default Spinner;
