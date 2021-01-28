58e37d093b0c383838429d08df2def989264f3b3
Seanbon0611 <seand0611@gmail.com>
2021-01-27 21:06:45 -0800

    fixed not found page for mobile view

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -5,7 +5,7 @@ import "./not-found.styles.scss";
 const NotFound = () => {
   return (
     <div className="not-found-container">
-      <h1 style={{ fontSize: "250px" }}>404</h1>
+      <h1 className="not-found-text">404</h1>
       <h1 style={{ padding: "10px 0px" }}>
         Oops! The page you are looking for does not exist.
       </h1>
