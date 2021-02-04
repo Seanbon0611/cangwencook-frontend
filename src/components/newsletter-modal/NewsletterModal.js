1e0fb0066ce93e8cc0488e0e2a07d240c13f1ea5
Seanbon0611 <seand0611@gmail.com>
2021-02-03 18:37:29 -0800

    fixed modal when submitting email to prevent default action

--- src/components/newsletter-modal/NewsletterModal.js
+++ src/components/newsletter-modal/NewsletterModal.js
@@ -4,43 +4,51 @@ import NewsletterForm from "../newsletter-form/NewsletterForm";
 import Socials from "../socials/Socials";
 import "./newsletter-modal.styles.scss";
 
-const NewsletterModal = ({ isShowing, hide }) =>
-  isShowing
-    ? ReactDOM.createPortal(
-        <>
-          <div className="modal-overlay">
-            <div
-              className="modal-wrapper"
-              aria-modal
-              aria-hidden
-              tabIndex={-1}
-              role="dialog"
-            >
-              <div className="modal">
-                <div className="modal-header">
-                  <span
-                    aria-hidden="true"
-                    className="modal-close-button"
-                    onClick={hide}
-                  >
-                    &times;
-                  </span>
-                  <form className="modal-content">
-                    <h3>Don't Miss Out!</h3>
-                    <p>
-                      Be the first to know about new recipes, seasonal specials
-                      and more!
-                    </p>
-                    <NewsletterForm />
-                  </form>
-                </div>
-                <Socials />
+const NewsletterModal = ({ isShowing, hide }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    hide();
+  };
+
+  if (isShowing) {
+    return ReactDOM.createPortal(
+      <>
+        <div className="modal-overlay">
+          <div
+            className="modal-wrapper"
+            aria-modal
+            aria-hidden
+            tabIndex={-1}
+            role="dialog"
+          >
+            <div className="modal">
+              <div className="modal-header">
+                <span
+                  aria-hidden="true"
+                  className="modal-close-button"
+                  onClick={hide}
+                >
+                  &times;
+                </span>
+                <form className="modal-content" onSubmit={handleSubmit}>
+                  <h3>Don't Miss Out!</h3>
+                  <p>
+                    Be the first to know about new recipes, seasonal specials
+                    and more!
+                  </p>
+                  <NewsletterForm />
+                </form>
               </div>
+              <Socials />
             </div>
           </div>
-        </>,
-        document.body
-      )
-    : null;
+        </div>
+      </>,
+      document.body
+    );
+  } else {
+    return null;
+  }
+};
 
 export default NewsletterModal;
