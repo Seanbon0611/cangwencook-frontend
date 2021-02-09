c06bbd2c00c266e6771d62c895b2984275aa14c5
Seanbon0611 <seand0611@gmail.com>
2021-02-08 18:58:09 -0800

    added spinner compoennet when recipe is loading

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import RecipeCard from "../../components/recipe-card/RecipeCard";
 import RecipeCarousel from "../../components/recipe-carousel/RecipeCarousel";
+import Spinner from "../../components/spinner/Spinner";
 import "./homepage.styles.scss";
 
 const Homepage = ({ recipes }) => {
+  const [loading, setLoading] = useState(true);
+  recipes.length > 0 && setLoading(false);
   const topThree = recipes.slice(0, 3);
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -21,9 +24,9 @@ const Homepage = ({ recipes }) => {
       </div>
       <h1 className="latest-recipes">Latest Recipes</h1>
       <div className="top-recipes">
-        {topThree.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+        {topThree.map((recipe) =>
+          loading ? <Spinner /> : <RecipeCard key={recipe.id} recipe={recipe} />
+        )}
       </div>
       <div className="top-recipes-mobile">
         <RecipeCarousel recipes={topThree} />
