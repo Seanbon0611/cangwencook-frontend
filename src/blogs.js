2f0deff5152ba4216f7e69dae8832a50d48b493d
Seanbon0611 <seand0611@gmail.com>
2021-03-26 21:49:57 -0700

    created file to set elements for blog index page

---
+++ src/blogs.js
@@ -0,0 +1,8 @@
+export const blogs = [
+  {
+    title: "Eggs",
+    preview:
+      "Here is a preview of the blog content, this right here is just a filler",
+    thumbnail: "link to thumbnail",
+  },
+];
