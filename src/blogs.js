d2822235973101e3b621324d6ea86f68d8d3b8d4
Seanbon0611 <seand0611@gmail.com>
2021-03-27 19:56:25 -0700

    added path as an attribute

--- src/blogs.js
+++ src/blogs.js
@@ -2,7 +2,9 @@ export const blogs = [
   {
     title: "Eggs",
     preview:
-      "Here is a preview of the blog content, this right here is just a filler",
-    thumbnail: "link to thumbnail",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius natus esse necessitatibus dolorem, saepe voluptatibus distinctio, perferendis totam ea porro molestias eligendi praesentium cupiditate repellendus, dolores aut. Vitae, temporibus consequatur.",
+    thumbnail:
+      "https://images.unsplash.com/photo-1582722872445-44dc5f7e3c8f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80",
+    path: "egg-blog",
   },
 ];
