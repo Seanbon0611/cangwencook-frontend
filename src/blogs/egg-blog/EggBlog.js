675160f6456e67349e99dc1ed73a2e8a44cfe395
Seanbon0611 <seand0611@gmail.com>
2021-04-19 16:52:04 -0700

    updated Egg blog with all content(styles satill need to be applied)

--- src/blogs/egg-blog/EggBlog.js
+++ src/blogs/egg-blog/EggBlog.js
@@ -1,8 +1,240 @@
 import React from "react";
-import { blogs } from "../../blogs";
+import "./egg-blog.styles.scss";
 
 const EggBlog = () => {
-  return <div>{blogs[0].content}</div>;
+  return (
+    <div className="egg-blog-container">
+      <h1>How well do you know your eggs?</h1>
+      <img
+        className="egg-thumbnail"
+        src="https://images.unsplash.com/photo-1582722872445-44dc5f7e3c8f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"
+      />
+      <p>
+        <strong>Did you know? According to </strong>
+        <a
+          href="https://www.healthline.com/nutrition/white-vs-brown-eggs#TOC_TITLE_HDR_3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>Healthline.com</strong>
+        </a>
+        <strong>...</strong>
+      </p>
+      <p>
+        <em>
+          Egg color depends on the breed of the chicken. For example, White
+          Leghorn chickens lay white-shelled eggs, while Plymouth Rocks and
+          Rhode Island Reds lay brown-shelled eggs (Source 1, 2)
+        </em>
+      </p>
+      <p>
+        <em>
+          Several studies have found that shell color has no significant effect
+          on egg quality and composition (3)
+        </em>
+      </p>
+      <p>
+        <strong>Oops, I ALMOST bought the same colored egg yolks!</strong>
+      </p>
+      <p>
+        One carton is labeled organic, pasture-raised eggs. The other carton is
+        labeled free-range eggs. (Open Nature is the brand name and has nothing
+        to do with the type of egg).&nbsp;
+      </p>
+      <p>
+        The two egg carton labels are clearly different, with distinctive
+        keywords labeled to help consumers differentiate the type of egg they
+        were going to purchase.
+      </p>
+      <p>
+        <strong>
+          The color of the egg yolk from both carton are both yellow but the
+          Organic, Pasture-Raised Egg is deeper in yellow
+        </strong>
+      </p>
+      <p>
+        <strong>Honestly&hellip;.</strong>
+      </p>
+      <p>
+        I thought the non-organic, free-range carton of eggs would have a
+        lighter or more yellower egg-yolk behind the brown-colored shell.
+      </p>
+      <p>
+        I am going to make it easier for you to compare the egg cartons without
+        using too many words. You can view the comparison chart I created below
+        to evaluate yourself.&nbsp;
+      </p>
+      <p>
+        I want to extend the opportunity for you to be a smarter consumer when
+        it comes to the type of eggs you like to eat. I&rsquo;m sure that many
+        consumers are confused as they walk down the aisles of their local
+        grocery store with only their{" "}
+        <strong>&lsquo;this is what I grew up on&rsquo;</strong> lens and
+        wanting to save a few dollars to end up with a lower quality carton of
+        eggs.
+      </p>
+      <p>
+        <em>
+          Note: I am not an egg expert. I am a learner and this is what
+          I&rsquo;ve learned about the various eggs. There is more in-depth
+          analysis of each category out there in the WWW. If you prefer this
+          type of learning, then please find Google them.
+        </em>
+      </p>
+      <p>
+        To help you quickly compare my two carton of eggs with cage-free and
+        commercially-farmed eggs{" "}
+        <span style={{ textDecoration: "underline" }}>
+          in the United States
+        </span>
+        <em>(EU countries have slightly different standard)</em>
+        <span style={{ textDecoration: "underline" }}>:</span>
+      </p>
+      <div className="table-container">
+        <strong>Category</strong>
+        <strong>Organic-Eggs</strong>
+        <strong>Free Range eggs</strong>
+        <strong>Cage-Free eggs</strong>
+        <strong>Commercially-farmed Non Organic Eggs</strong>
+        <strong>Color of egg</strong>
+        <span>Brown or White</span>
+        <span>Brown or White</span>
+        <span>Brown or White</span>
+        <span>Brown or White</span>
+        <strong>Cage</strong>
+        <span>Cage-Free</span>
+        <span>Cage-Free</span>
+        <span>Cage-Free</span>
+        <span>Crowded Cages</span>
+        <strong>Access to Outside Area</strong>
+        <span>Yes but no min. requirements on size of outside area</span>
+        <span>
+          Yes but no min. requirements on size of outside area; No guarantee
+          outdoor access
+        </span>
+        <span>No guarantee outdoor access, and many never see daylight.</span>
+        <span>None</span>
+        <strong>Pasture-Raised</strong>
+        <span>Free roaming</span>
+        <span>Free roaming</span>
+        <span>Free roaming within barns or covered chicken coops</span>
+        <span>None</span>
+        <strong>Feed</strong>
+        <span>
+          Organic grain, greens and insects as they can roam; No animal
+          by-product ever
+        </span>
+        <span>
+          Grain mixture but can supplement their diet with greens and insects as
+          they can roam
+        </span>
+        <span>Commercial feed</span>
+        <span>Commercial feed</span>
+        <strong>Antibiotics</strong>
+        <span>No</span>
+        <span>Use is unrestricted</span>
+        <span>Use is unrestricted</span>
+        <span>Use is unrestricted</span>
+        <strong>Use of Pesticide/Fertilizer</strong>
+        <span>No, min, Req is 3+ year of pesticide/fertilizer-free land</span>
+        <span>Possible</span>
+        <span>Highly Possible</span>
+        <span>Highly Possible</span>
+        <strong>Macronutrients</strong>
+        <span>Same</span>
+        <span>Same</span>
+        <span>Same</span>
+        <span>Same</span>
+      </div>
+      <p>
+        <strong>
+          What is the difference between organic and non-organic eggs?
+        </strong>
+      </p>
+      <p>
+        First and foremost, to receive the USDA organic stamp, farmers will need
+        to pass this set of requirements. Naturally, meeting a higher set of
+        standards could be inferred as a higher quality product. This stamp
+        confirms this hypothetical.
+      </p>
+      <p>
+        According to{" "}
+        <a
+          href="https://www.dovemed.com/healthy-living/wellness-center/are-organic-eggs-healthier-regular-eggs/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          DovMed.com
+        </a>
+      </p>
+      <p>
+        <em>
+          Organic food certification is a self-regulated term depending on the
+          national government that regulates it. In the United States, organic
+          foods must not have any artificial food additives or genetically
+          modified ingredients. In addition, organic foods must refrain from
+          artificial methods, materials, and conditions like chemical ripening
+          and food irradiation. Organic farmers can use pesticides as long as
+          they are not synthetic. However, if pests and weeds are too difficult
+          to manage through organic pesticides or herbicides, synthetic
+          substances that are approved on the National List of synthetic
+          substances can be used.
+        </em>
+      </p>
+      <p>
+        <strong>Heathline.com: </strong>
+        <a
+          href="https://www.healthline.com/nutrition/white-vs-brown-eggs#TOC_TITLE_HDR_3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://www.healthline.com/nutrition/white-vs-brown-eggs#TOC_TITLE_HDR_3
+        </a>
+      </p>
+      <p>
+        <strong>DovMed.com: </strong>
+        <a
+          href="https://www.dovemed.com/healthy-living/wellness-center/are-organic-eggs-healthier-regular-eggs/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>
+            https://www.dovemed.com/healthy-living/wellness-center/are-organic-eggs-healthier-regular-eggs/
+          </strong>
+        </a>
+      </p>
+      <p>
+        <strong>Source 1: </strong>
+        <a
+          href="https://pubmed.ncbi.nlm.nih.gov/22742508/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>https://pubmed.ncbi.nlm.nih.gov/22742508/</strong>
+        </a>
+      </p>
+      <p>
+        <strong>Source 2: </strong>
+        <a
+          href="https://pubmed.ncbi.nlm.nih.gov/11194033/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>https://pubmed.ncbi.nlm.nih.gov/11194033/</strong>
+        </a>
+      </p>
+      <p>
+        <strong>Source 3: </strong>
+        <a
+          href="https://pubmed.ncbi.nlm.nih.gov/20181877/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>https://pubmed.ncbi.nlm.nih.gov/20181877/</strong>
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default EggBlog;
