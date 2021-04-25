069d9545a5d17c12ada6f226ae8b43149a6b13b4
Seanbon0611 <seand0611@gmail.com>
2021-04-24 19:37:31 -0700

    formateted blog index for both mobile & desktop

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -2,6 +2,7 @@ import React, { useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import TagInstagram from "../../components/tag-ig/TagInstagram";
 import RecipeCarousel from "../../components/recipe-carousel/RecipeCarousel";
+import RecipeCard from "../../components/recipe-card/RecipeCard";
 import "./blog.styles.scss";
 import { blogs } from "../../blogs";
 import EggBlog from "../../blogs/egg-blog/EggBlog";
@@ -18,29 +19,50 @@ const Blog = ({ recipes, setCurrentPage }) => {
     return () => setCurrentPage("");
   }, [setCurrentPage, memoizedTrigger]);
   const topThree = recipes.slice(0, 3);
+  const topTwo = recipes.slice(0, 2);
   return (
     <div className="blog-page-container">
       <h1 className="page-title">Blog</h1>
-      <div className="blog-preview-container">
-        {blogs.map((blog) => (
-          <div className="blog-preview" key={blog.title}>
-            <Link className="thumbnail-text" to={`/blog/${blog.path}`}>
-              <img
-                className="blog-thumbnail"
-                src={blog.thumbnail}
-                alt="egg blog"
-              />
-              <div>
-                <h3 style={{ paddingLeft: "10px" }}>{blog.title}</h3>
-                <p className="blog-preview-text">{blog.preview}</p>
-              </div>
+      <div className="blog-about-grid">
+        <div className="blog-preview-container">
+          {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.title}>
+              <Link className="thumbnail-text" to={`/blog/${blog.path}`}>
+                <img
+                  className="blog-thumbnail"
+                  src={blog.thumbnail}
+                  alt="egg blog"
+                />
+                <div>
+                  <h3 style={{ paddingLeft: "10px" }}>{blog.title}</h3>
+                  <p className="blog-preview-text">{blog.preview}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="about-top-recipes">
+          <h2 className="about-text">About Gwen</h2>
+          <img
+            style={{ width: "189px", height: "189px" }}
+            src="https://can-gwen-cook.s3-us-west-1.amazonaws.com/cgc-about-me.png"
+            alt="about-me-pic"
+          />
+          <p style={{ paddingBottom: "50px" }}>
+            Hi, my name is Gwen welcome to my collection of healthy and tasty
+            recipes!{" "}
+            <Link style={{ textDecoration: "underline" }} to="/about">
+              read more
             </Link>
-          </div>
-        ))}
+          </p>
+          {topTwo.map((recipe) => {
+            return <RecipeCard recipe={recipe} />;
+          })}
+        </div>
       </div>
       <TagInstagram />
-      <div className="about-me-pic-container">
-        <h2>About Gwen</h2>
+      <div className="about-me-pic-container-mobile">
+        <h2 className="about-text">About Gwen</h2>
         <img
           style={{ width: "189px", height: "189px" }}
           src="https://can-gwen-cook.s3-us-west-1.amazonaws.com/cgc-about-me.png"
